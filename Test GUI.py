873198a751bd2ecb49e2299c56a5719ce19f98d2
Mijan0612 <116630534+Mijan0612@users.noreply.github.com>
2023-03-23 03:05:30 +0800

    New way of writing the gui
    I separate the feature extraction and preprocessing in function so that we can call later on, later ill add on how to show the signals. The signals is mainly there to show whether the upload functions or not

--- Test GUI.py
+++ Test GUI.py
@@ -1,7 +1,8 @@
 import base64
 import io
+import plotly.express as px
 
-
+import warnings
 import dash
 import pandas as pd
 from dash import dcc,html,Dash
@@ -11,15 +12,130 @@ import plotly.graph_objs as go
 import mne
 import pickle
 import numpy as np
+from mne_features.feature_extraction import extract_features
 
-app = Dash(__name__)
 
-app.layout = html.Div([
-    dcc.Tabs([
-        dcc.Tab(label='Upload EDF', children=[
+def preprocessed_data(data):
+
+
+    event_id = {'Sleep stage W': 1,
+                'Sleep stage 1': 2,
+                'Sleep stage 2': 3,
+                'Sleep stage 3': 4,
+                'Sleep stage 4': 4,
+                'Sleep stage R': 5}
+
+    # Read the PSG data, raw contains the following:
+    # 2d numpy array for data
+    # times, info
+    # annotations
+    raw = mne.io.read_raw_edf(data, stim_channel='marker', misc=['rectal'])
+    #load data into memory
+    raw.load_data()
+
+    # Select only EEG
+    raw.drop_channels(['EOG horizontal', 'Resp oro-nasal', 'EMG submental', 'Temp rectal',
+                       'Event marker'])
+
+    # Extract feature names from channel names
+    #feature_names = raw.ch_names
+
+    # Filter the high-pass frequency to be 49 and low-pass to 2
+    raw.filter(l_freq=2, h_freq=49)
+
+    # making a dictionary for scalings where eeg is the key and the value is 40e-5
+    scalings = {'eeg': 40e-5}
+    #raw.plot(duration=60, scalings=scalings, remove_dc=False, )
+    tmax = 30. - 1. / raw.info['sfreq']  # Epoch size
+
+
+    events, _ = mne.events_from_annotations(raw, event_id=event_id, chunk_duration=30.)
+    # u, indices = np.unique(annot['description'], return_index=True)
+    # Create epochs of 30 sec from the continuous signal
+    epochs = mne.Epochs(raw=raw, events=events, event_id=event_id, tmin=0., tmax=tmax, baseline=None)
+
+def get_sleep_stages(epochs):
+    """
+    Returns an array of sleep stage labels corresponding to each epoch in the given MNE Epochs object.
+    """
+    # Assuming you have access to the sleep stage information in your code, you can get it using the following:
+    sleep_stage_data = epochs.events[:, 2]
+    sleep_stages = []
+
+    # Map the sleep stage codes to their corresponding labels
+    for stage in sleep_stage_data:
+        if stage == 1:
+            sleep_stages.append('1')
+        elif stage == 2:
+            sleep_stages.append('2')
+        elif stage == 3:
+            sleep_stages.append('3')
+        elif stage == 4:
+            sleep_stages.append('4')
+        elif stage == 5:
+            sleep_stages.append('5')
+        #elif stage == 6:
+        #    sleep_stages.append('5')
+        else:
+            sleep_stages.append('UNKNOWN')
+
+    return sleep_stages
+
+def feature_extract(epochs):
+    #specific frequency bands
+    FREQ_BANDS = {"delta": [0.5, 4.5],
+                  "theta": [4.5, 8.5],
+                  "alpha": [8.5, 11.5],
+                  "sigma": [11.5, 15.5],
+                  "beta": [15.5, 30],
+                  }  # no gamma because it has freq higher than Ntquist frequency
+
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+
+    selected_features = ['pow_freq_bands']
+
+    freq_bands = np.unique(np.concatenate(list(map(list, (FREQ_BANDS.values())))))
+
+    funcs_params = dict(pow_freq_bands__normalize=False,
+                        pow_freq_bands__ratios='all',
+                        pow_freq_bands__psd_method='fft',
+                        pow_freq_bands__freq_bands=freq_bands)
+
+    sfreq = epochs.info['sfreq']
+    features_all = extract_features(epochs.get_data(),
+                                    sfreq,
+                                    selected_funcs=selected_features,
+                                    return_as_df=True,
+                                    funcs_params=funcs_params)
+
+    # Get the subject ID
+    file_name = epochs.info['subject_info']['id']
+    subject_name = file_name[:8]
+
+
+    # Get the sleep stage information, assuming it's available
+    sleep_stages = get_sleep_stages(epochs)  # replace this with your own function that gets the sleep stages
+
+    # create a new dataframe with the data, sleep stage, and subject_id columns
+    data = pd.DataFrame(features_all)
+    data['sleep_stage'] = sleep_stages
+    data['subject_id'] = subject_name
+
+
+    # display the data in a table-like view
+    #print(data.to_string(index=False))
+
+    return data
+
+app = Dash(__name__)    # create Dash app
+app.title = 'Sleepiness Detection'  # set title
+server = app.server    # set server
+
+app.layout = html.Div([ # define layout
+    dcc.Tabs([  # define tabs
+        dcc.Tab(label='Upload EDF', children=[ # define tab for uploading edf
             dcc.Upload(
                 id='upload-edf',
-                accept='.edf',
                 children=html.Div([
                     'Drag and Drop or ',
                     html.A('Select Files')
@@ -37,7 +153,7 @@ app.layout = html.Div([
             ),
             html.Div(id='output-data-upload')
         ]),
-        dcc.Tab(label='EEG Signals', children=[
+        dcc.Tab(label='EEG Signals', children=[ # define tab for eeg signals
             dcc.Graph(id='eeg-graph'),
             html.Div(id='warning')
         ]),
@@ -47,111 +163,19 @@ app.layout = html.Div([
     ])
 ])
 
-#below is before i add the preprocessing part
-'''
 @app.callback(Output('output-data-upload', 'children'),
-              Input('upload-edf', 'contents'),
-              State('upload-edf', 'filename'))
-def update_output(content, filename):
-    
-    if content is not None:
+                Input('upload-edf', 'contents'),
+                State('upload-edf', 'filename'))
 
-        # Read EDF file
+def update_output(contents, filename):
+
+    if contents is not None:
+        processed_data = preprocessed_data(contents)
+
+        extracted_features = feature_extract(processed_data)
+    return html.Div(id = 'featured_data')
 
 
-        return html.Div([
-            html.H5(filename),
-            html.P('Data uploaded and preprocessed successfully!')
-        ])
-
-    else:
-        return html.Div([
-            html.P('Upload EDF file to begin')
-        ])
-
-@app.callback(Output('eeg-graph', 'figure'),
-              Input('output-data-upload', 'children'))'''
-
-#below is after putting preprocesing
-
-@app.callback(Output('output-data-upload', 'children'),
-              Input('upload-edf', 'contents'),
-              State('upload-edf', 'filename'))
-def update_output(content, filename):
-
-        event_id = {'Sleep stage W': 1,
-                    'Sleep stage 1': 2,
-                    'Sleep stage 2': 3,
-                    'Sleep stage 3': 4,
-                    'Sleep stage 4': 4,
-                    'Sleep stage R': 5}
-
-        if content is not None:
-            # Read EDF file
-            content_type, content_string = content.split(',')
-            decoded = base64.b64decode(content_string)
-            file_like_object = io.BytesIO(decoded)
-
-            raw = mne.io.read_raw_edf(file_like_object, stim_channel='marker', misc=['rectal'])
-            raw.load_data()
-
-            scalings = dict(eeg=40e-5)
-            raw.plot(duration=60, scalings=scalings, remove_dc=False, )
-            tmax = 30. - 1. / raw.info['sfreq']  # Epoch size
-            events, _ = mne.events_from_annotations(raw, event_id=event_id, chunk_duration=30.)
-
-            # Preprocess data
-            raw.drop_channels(['EOG horizontal', 'Resp oro-nasal', 'EMG submental', 'Temp rectal', 'Event marker'])
-            raw.filter(l_freq=2, h_freq=49)
-            epochs = mne.Epochs(raw=raw, events=events, event_id=event_id, tmin=0., tmax=tmax, baseline=None)
-
-            # Save preprocessed data to file
-            with open("preprocessed_data.pkl", "wb") as f:
-                pickle.dump({"data": epochs.get_data(), "labels": epochs.events[:, 2]}, f)
-
-            return html.Div([
-                html.H5(filename),
-                html.P('Data uploaded and preprocessed successfully!')
-            ])
-        else:
-            return html.Div([
-                html.P('Upload EDF file to begin')
-            ])
-
-@app.callback(Output('eeg-graph', 'figure'),
-              Input('output-data-upload', 'children'))
-
-def update_eeg_graph(data):
-
-    with open("preprocessed_data.pkl", "rb") as f:
-        preprocessed_data = pickle.load(f)
-
-# access the preprocessed data and labels
-    data = preprocessed_data["data"]
-    labels = preprocessed_data["labels"]
-
-    # create the plotly figure object
-    fig = go.Figure()
-    for i in range(data.shape[0]):
-        fig.add_trace(go.Scatter(x=np.arange(data.shape[1])/100, y=data[i], name=f"Channel {i+1}"))
-
-    fig.update_layout(title='EEG Signals')
-
-    return fig
-
-@app.callback(Output('warning', 'children'),
-              Input('eeg-graph', 'figure'))
-def check_sleep_stage_threshold(figure):
-    # Determine sleep stage threshold and check if exceeded
-    exceeded = True
-    # ...
-    if exceeded:
-        return html.Div([
-            html.P('WARNING: WAKE UP, YOU ARE DRIVING')
-        ])
-    else:
-        return html.Div([])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
