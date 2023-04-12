141ed7bfbf57c46043053326da460d2d88de27bd
Mijan0612 <116630534+Mijan0612@users.noreply.github.com>
2023-04-12 14:39:19 +0800

    Update Main Gui.py

--- Main Gui.py
+++ Main Gui.py
@@ -1,11 +1,8 @@
 from dash import Dash, html, dcc
-import dash
 import dash_bootstrap_components as dbc
+import dash
 
-#RUN THIS FILE FOR GUI
-
-app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SPACELAB])
-
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SLATE])
 sidebar = dbc.Nav(
     [
         dbc.NavLink(
@@ -17,33 +14,32 @@ sidebar = dbc.Nav(
         )
         for page in dash.page_registry.values()
     ],
-    vertical="true",
-    pills="true",
+    vertical=True,
+    pills=True,
+    className='body',
 )
 
-app.layout = html.Div([
-    dbc.Row(
-        html.H1('Sleep Stages App example'),
-    ),
+app.layout = dbc.Container(
+    [
+        dbc.Row([
+            dbc.Col(html.H1("Sleep Stages App example", style={'textAlign': 'center'}, className='header')),
+        ]),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        sidebar
+                    ], xs=2, sm=2, md=1, lg=1, xl=1, xxl=1, style={'height': '99%'}),
+                dbc.Col(
+                    [
+                        dash.page_container
+                    ], xs=9, sm=9, md=11, lg=11, xl=11, xxl=11)
+            ]
+        )
 
-    # html.Div(
-    #     [
-    #         html.Div(
-    #             dcc.Link(
-    #                 f" [{page['name']}] ", href=page['path']
-    #             )
-    #         )
-    #         for page in dash.page_registry.values()
-    #     ]
-    # ),
-    dbc.Row(
-        dash.page_container
-    ),
-    html.Hr(),
-    dbc.Row(
-        sidebar,
-    ),
-])
+    ], fluid=True
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
